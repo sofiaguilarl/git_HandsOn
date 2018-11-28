3373f5deb6d96729f7c3779f30c3a93bfec93579
Sofia <sofiaguilar.95@gmail.com>
2018-11-28 18:00:29 +0100

    find simple motifs in our sequences

--- seqClass.sh
+++ seqClass.sh
@@ -13,5 +13,13 @@ if [[ $seq =~ ^[ACGTU]+$ ]]; then
 else
   echo "The sequence is not DNA nor RNA"
 fi
-
+motif=$(echo $2 | tr a-z A-Z)
+if [[ -n $motif ]]; then
+  echo -en "Motif search enabled: looking for motif '$motif' in sequence '$seq'... "
+  if [[ $seq =~ $motif ]]; then
+    echo "FOUND"
+  else
+    echo "NOT FOUND"
+  fi
+fi
 
