adc623b258bdd9bc852d05552646af8e74a15da2
Sofia <sofiaguilar.95@gmail.com>
2018-11-28 18:07:47 +0100

    he modificado el found

--- seqClass.sh
+++ seqClass.sh
@@ -17,7 +17,7 @@ motif=$(echo $2 | tr a-z A-Z)
 if [[ -n $motif ]]; then
   echo -en "Motif search enabled: looking for motif '$motif' in sequence '$seq'... "
   if [[ $seq =~ $motif ]]; then
-    echo "FOUND"
+    echo "ENCONTRADO JEJE"
   else
     echo "NOT FOUND"
   fi
