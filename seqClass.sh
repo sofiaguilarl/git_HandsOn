fa1adaf1bc141e2c70cc4640308d5f3bc9ddb927
Sofia <sofiaguilar.95@gmail.com>
2018-11-28 18:33:40 +0100

    test merge

--- seqClass.sh
+++ seqClass.sh
@@ -17,7 +17,6 @@ motif=$(echo $2 | tr a-z A-Z)
 if [[ -n $motif ]]; then
   echo -en "Motif search enabled: looking for motif '$motif' in sequence '$seq'... "
   if [[ $seq =~ $motif ]]; then
-    echo "Encontrado yuju"
   else
     echo "NO LO HE ENCONTRADO JEJE"
   fi
